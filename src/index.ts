f175ad5d1672708a2fb4750821227aa736434694
Chris Stone <chstone@microsoft.com>
2017-05-13 07:26:43 -0400

    fix exports

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export * from './dialogs/speech-dialog';
+export * from './dialogs/luis-dialog';
 export * from './workflow/recognize-speech-action';
 export * from './workflow/understand-speech-action';
